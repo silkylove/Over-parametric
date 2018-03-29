851c6c03c89020e6e6397a25ecbd0b2d760ace32
huang <353837214@qq.com>
2018-03-29 15:39:17 +0800

    add

--- over-param.py
+++ over-param.py
@@ -13,7 +13,7 @@ import numpy as np
 from torch.autograd import Variable
 from torch import nn, optim
 from torch.utils.data import DataLoader, Dataset
-from torchvision import transforms, datasets, models
+from torchvision import transforms, models
 
 use_gpu = torch.cuda.is_available()
 
@@ -159,74 +159,57 @@ def accuracy(output, target, topk=(1,)):
     return res
 
 
-def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+def train_model(model, criterion, optimizer, scheduler, num_epochs=60):
     since = time.time()
-
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_acc = 0.0
 
     for epoch in range(num_epochs):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
 
-        # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                scheduler.step()
-                model.train(True)  # Set model to training mode
+        scheduler.step()
+        model.train(True)
+
+        running_loss = 0.0
+        running_corrects = 0
+
+        for data in training_loader:
+            inputs, labels = data
+            if use_gpu:
+                inputs = Variable(inputs.cuda())
+                labels = Variable(labels.cuda())
             else:
-                model.train(False)  # Set model to evaluate mode
+                inputs, labels = Variable(inputs), Variable(labels)
 
-            running_loss = 0.0
-            running_corrects = 0
+            optimizer.zero_grad()
 
-            # Iterate over data.
-            for data in dataloaders[phase]:
-                # get the inputs
-                inputs, labels = data
+            outputs = model(inputs)
+            _, preds = torch.max(outputs.data, 1)
+            loss = criterion(outputs, labels)
 
-                # wrap them in Variable
-                if use_gpu:
-                    inputs = Variable(inputs.cuda())
-                    labels = Variable(labels.cuda())
-                else:
-                    inputs, labels = Variable(inputs), Variable(labels)
+            loss.backward()
+            optimizer.step()
 
-                # zero the parameter gradients
-                optimizer.zero_grad()
+            running_loss += loss.data[0] * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data)
 
-                # forward
-                outputs = model(inputs)
-                _, preds = torch.max(outputs.data, 1)
-                loss = criterion(outputs, labels)
+        epoch_loss = running_loss / len(training_dataset)
+        epoch_acc = running_corrects / len(training_dataset)
 
-                # backward + optimize only if in training phase
-                if phase == 'train':
-                    loss.backward()
-                    optimizer.step()
-
-                # statistics
-                running_loss += loss.data[0] * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
-
-            epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_acc = running_corrects / dataset_sizes[phase]
-
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-                phase, epoch_loss, epoch_acc))
-
-            # deep copy the model
-            if phase == 'val' and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model_wts = copy.deepcopy(model.state_dict())
+        print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+            'train', epoch_loss, epoch_acc))
 
         print()
 
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(
         time_elapsed // 60, time_elapsed % 60))
-    print('Best val Acc: {:4f}'.format(best_acc))
 
-    # load best model weights
-    model.load_state_dict(best_model_wts)
     return model
+
+
+root = './'
+BATCH_SIZE = 100
+img_transforms = ImageTransformations(mode='random')
+label_transforms = LabelTransformations(mode='random')
+training_dataset = CIFAR10(root, train=True, transform=img_transforms, target_transform=label_transforms)
+training_loader = DataLoader(training_dataset, BATCH_SIZE, shuffle=True, pin_memory=True)
