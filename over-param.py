eb266293e6dbfa7ae80cc67041ba4192ce500635
huang <353837214@qq.com>
2018-04-01 22:39:08 +0800

    add

--- over-param.py
+++ over-param.py
@@ -50,7 +50,7 @@ def train_model(model, criterion, optimizer, scheduler, log_saver, num_epochs=60
             optimizer.step()
 
             loss_meter.update(loss.data[0], outputs.size(0))
-            acc_meter.update(accuracy(outputs.data, labels.data))
+            acc_meter.update(accuracy(outputs.data, labels.data)[-1][0], outputs.size(0))
             steps += 1
 
             log_saver.log(steps, loss_meter.avg, acc_meter.avg)
@@ -78,15 +78,16 @@ def train_model(model, criterion, optimizer, scheduler, log_saver, num_epochs=60
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(
         time_elapsed // 60, time_elapsed % 60))
-    return log_saver
+    return model, log_saver
 
 
 if __name__ == "__main__":
     root = './'
     BATCH_SIZE = 128
     weight_decay = 0.
-    img_transforms = ImageTransformations(mode='random')
-    label_transforms = LabelTransformations(mode='random')
+    mode = ['random', None]
+    img_transforms = ImageTransformations(mode=mode[0])
+    label_transforms = LabelTransformations(mode=mode[1])
     training_dataset = CIFAR10(root, train=True, transform=img_transforms, target_transform=label_transforms)
     training_loader = DataLoader(training_dataset, BATCH_SIZE, shuffle=True, pin_memory=True)
 
@@ -95,9 +96,13 @@ if __name__ == "__main__":
     alex = alexnet.alexnet()
     inception = inceptions.GoogLeNet()
 
+    model = resnet18
+    if use_gpu:
+        model = resnet18.cuda()
+
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(resnet18.parameters(), lr=0.01, momentum=0.9, weight_decay=weight_decay)
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=weight_decay)
     exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.95)
     log = Logger('train')
 
-    log = train_model(resnet18, criterion, optimizer, exp_lr_scheduler, log, num_epochs=60)
+    model, log = train_model(model, criterion, optimizer, exp_lr_scheduler, log, num_epochs=10)
